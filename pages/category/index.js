65185012315bcf02f3a84072e8d4604c8d6bd23a
apple <shuimu0579@gmail.com>
2020-11-30 23:34:24 +0800

    change

--- pages/category/index.js
+++ pages/category/index.js
@@ -12,7 +12,7 @@ Page({
       categoriesData = categoriesData.data.data;
     }
     console.log(categoriesData);
-
+    // const titles = ['热搜推荐', '手机数码', '家用电器','生鲜果蔬', '酒水饮料']
     const vtabs = categoriesData.map(item => ({title: item.category_name, id: item.id}))
     this.setData({vtabs})
   },
