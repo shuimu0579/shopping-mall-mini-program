422e09d232131acc38b2b631f310dc25577ba314
apple <shuimu0579@gmail.com>
2020-12-19 15:33:24 +0800

    change

--- pages/category/index.js
+++ pages/category/index.js
@@ -13,9 +13,9 @@ Page({
     })
     let goodsId = e.currentTarget.dataset.id 
     let goods = await wx.wxp.request({
-      //10.8.1.16  是本机ip 就是localhost  mac下 ifconfig en0 --> inet 后面就是
+      //10.8.1.117  是本机ip 就是localhost  mac下 ifconfig en0 --> inet 后面就是
       // url: `http://localhost:3000/goods/goods/${goodsId}`, 
-      url: `http://10.8.1.16:3000/goods/goods/${goodsId}`, 
+      url: `http://10.8.1.117:3000/goods/goods/${goodsId}`, 
     })
     console.log(goods);
     
@@ -36,7 +36,7 @@ Page({
       title: 'Loading..',
     })
     let categoriesData = await wx.wxp.request({
-      url: 'http://10.8.1.16:3000/goods/categories',
+      url: 'http://10.8.1.117:3000/goods/categories',
     })
     if (categoriesData){
       categoriesData = categoriesData.data.data;
@@ -129,7 +129,7 @@ Page({
       }
     }
     let goodsData = await wx.wxp.request({
-      url: `http://10.8.1.16:3000/goods/goods?page_index=${pageIndex}&page_size=${pageSize}&category_id=${categoryId}`,
+      url: `http://10.8.1.117:3000/goods/goods?page_index=${pageIndex}&page_size=${pageSize}&category_id=${categoryId}`,
     })
     if (goodsData){
       goodsData = goodsData.data.data;
