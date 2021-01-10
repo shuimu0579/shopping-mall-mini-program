34b8bcc1301034371761014136f08c7dc8d4a349
apple <shuimu0579@gmail.com>
2021-01-10 11:51:59 +0800

    change

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 //app.js
 import wxp from './lib/wxp'
+import Event from './lib/event2'
 
+// getApp().globalEvent
 App({
   wxp: (wx.wxp = wxp),
+  globalData: (wx.globalData = {}),
+  globalEvent: (wx.globalEvent = new Event()),
   onLaunch: function () {
     
     if (!wx.cloud) {
